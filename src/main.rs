381e64713af2c1fcd4f60f7b397e3f474b32364b
Anthony McClay <anthony.mcclay@gmail.com>
2026-01-17 06:28:11 -0600

    return struct in function

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,34 @@ fn main() {
 
     struct_immutable();
     struct_mutable();
+    return_instance();
 
 }
+
+fn return_instance() {
+    println!("\n\n\tReturn Instance\n\n");
+
+    let mut user1 = build_user(String::from("bigmanuser"),
+                               String::from("bigman@example.com"));
+
+    println!("email address: {}", user1.email);
+    println!("username: {}", user1.username);
+    println!("active: {}", user1.active);
+    println!("sign in count: {}", user1.sign_in_count);
+
+    user1.sign_in_count += 5;
+    println!("sign in count: {}", user1.sign_in_count);
+}
+
+fn build_user(email: String, username:String) -> User {
+    User {
+        active: true,
+        sign_in_count: 1,
+        username,
+        email,
+    }
+}
+
 
 fn struct_mutable() {
     println!("\n\n\tStruct Mutable\n\n");
