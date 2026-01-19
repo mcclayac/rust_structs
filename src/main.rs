00b0c94b4793100f16dda7064a60778725fb3627
Anthony McClay <anthony.mcclay@gmail.com>
2026-01-19 05:14:44 -0600

    structs and ownership

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,53 @@ fn main() {
     struct_immutable();
     struct_mutable();
     return_instance();
+    different_types_with_tuple_struct();
+    defining_unit_like_struct();
+    ownership_of_struct_data();
+
+}
+
+// this won't work without lifetimes
+// struct user2 {
+//     active: bool,
+//     username: &str,
+//     email: &str,
+//     sign_in_count: u64,
+// }
+fn ownership_of_struct_data() {
+    println!("\n\nOwnership of Struct Data\n");
+//     let user1 = User2 {
+//         active: true,
+//         username: "someusername123",
+//         email: "someone@example.com",
+//         sign_in_count: 1,
+//     };
+}
+
+struct AlwaysEqual;
+
+fn defining_unit_like_struct() {
+    println!("Defining Unit-Like Structs");
+
+    let subject = AlwaysEqual;
+
+
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32,i32,i32);
+
+fn different_types_with_tuple_struct() {
+    println!("\n\n\tDifferent Types with tuple structs.\n\n");
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let Point(x, y, z) = origin;
+    println!("Point x{}, y{}, z{}", x, y, z);
+
+    let Color(a, b, c) = black;
+    println!("Color a {}, b {}, c{}", a, b, c);
 
 }
 
@@ -22,6 +69,33 @@ fn return_instance() {
 
     user1.sign_in_count += 5;
     println!("sign in count: {}", user1.sign_in_count);
+
+    println!("\nCreating User2 ");
+
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: Strring::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    let user2 =  User {
+        email: String::from("another2@example.com"),
+        ..user1
+    };
+
+    println!("user2.username: {}", user2.username);
+
+    println!("--------------");
+    println!("Trying to re-print user1, but cannot because username has been used");
+    println!("email address: {}", user1.email);
+    // println!("username: {}", user1.username);
+    // Value used after being moved [E0382]
+
+    println!("active: {}", user1.active);
+    println!("sign in count: {}", user1.sign_in_count);
+
+
 }
 
 fn build_user(email: String, username:String) -> User {
